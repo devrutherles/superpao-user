a83894596ac9a41a7ebc19c5ccc3297a6ebeb47d
Bruno Matheus <rutherles@gmail.com>
2024-02-29 10:10:17 -0300

    up59

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve firebase messaging
+// Retrieve firebase messagingppp
 const messaging = firebase.messaging();
 
 // Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
